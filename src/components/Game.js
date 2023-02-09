237f5611bc0b53d6a4ab3661e8fb871429ca44a0
Alessandro Saggese <asaggese@gmail.com>
2023-02-09 19:08:14 +0100

    Update Game.js

--- src/components/Game.js
+++ src/components/Game.js
@@ -6,10 +6,14 @@ const Game = () => {
     const [currentPlayer, setCurrentPlayer] = useState("X");
     const [gameOver, setGameOver] = useState(false);
     const [score, setScore] = useState({ X: 0, O: 0 });
+    const [winner, setWinner] = useState(null);
+    const [showModal, setShowModal] = useState(false);
 
     // Handle win
     const handleWin = (winner) => {
         setGameOver(true);
+        setWinner(winner);
+        setShowModal(true);
         setScore((prevScore) => ({ ...prevScore, [winner]: prevScore[winner] + 1 }));
     }
 
@@ -17,14 +21,24 @@ const Game = () => {
     const handleReset = () => {
         setCurrentPlayer("X");
         setGameOver(false);
+        setWinner(null);
+        setShowModal(false);
     }
 
     return (
-        <div style={{textAlign: "center"}}>
+        <div style={{ textAlign: "center" }}>
             <h1>Tic Tac Toe</h1>
             <p>Current player: {currentPlayer}</p>
             <p>Score: X: {score.X} O: {score.O}</p>
             <Board currentPlayer={currentPlayer} gameOver={gameOver} onWin={handleWin} onReset={handleReset} setCurrentPlayer={setCurrentPlayer} />
+            {showModal && (
+                <div style={{ position: "fixed", top: "0", left: "0", right: "0", bottom: "0", backgroundColor: "rgba(0,0,0,0.5)", display: "flex", justifyContent: "center", alignItems: "center" }}>
+                    <div style={{ backgroundColor: "white", padding: "20px", borderRadius: "10px" }}>
+                        <h2>{winner} wins!</h2>
+                        <button onClick={handleReset}>Play again</button>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
